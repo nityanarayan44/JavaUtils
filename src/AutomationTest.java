9c080f2e56b344bd08beb5e216039f6803d9423d
nityanarayan44 <nityanarayan44@live.com>
2017-11-22 11:51:57 +0530

    Automation Test Example updated

--- src/AutomationTest.java
+++ src/AutomationTest.java
@@ -17,25 +17,26 @@ public class AutomationTest extends Driver {
 		AutomationTest at = new AutomationTest();
 		
 		//Initiate the web driver
-		//WebDriver driver = at.getWebDriver("CHROME", "C:\\DRIVERS\\chromedriver_win32_new\\chromedriver.exe");
-		at.getWebDriver("FIREFOX", "E:\\MY-WORKPLACE\\__GIT_PROJECTS\\__Dependencies\\WebDrivers\\geckodriver.exe");
-		//WebDriver driver = at.getWebDriver("IE", "C:\\DRIVERS\\IEDriverServer_Win32_3.7.0\\IEDriverServer.exe");
-		//WebDriver driver = at.getWebDriver("EDGE", "C:\\DRIVERS\\IEDriverServer_x64_3.7.0\\IEDriverServer.exe");
+			//WebDriver driver = at.getWebDriver("CHROME", "C:\\DRIVERS\\chromedriver_win32_new\\chromedriver.exe");
+			at.getWebDriver("FIREFOX", "E:\\MY-WORKPLACE\\__GIT_PROJECTS\\__Dependencies\\WebDrivers\\geckodriver.exe");
+			//WebDriver driver = at.getWebDriver("IE", "C:\\DRIVERS\\IEDriverServer_Win32_3.7.0\\IEDriverServer.exe");
+			//WebDriver driver = at.getWebDriver("EDGE", "C:\\DRIVERS\\IEDriverServer_x64_3.7.0\\IEDriverServer.exe");
 		
 		// Operations on driver object 
-		at.maximizeBrowser();
+			at.maximizeBrowser();
 
-		at.driver.get("https://www.google.com?q=earth");
-		at.driver.switchTo().activeElement().sendKeys(Keys.ENTER);
-		//driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		// Operations
+			at.driver.get("https://www.google.com?q=earth");
+			at.driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+			//driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 		
 		//Wait
-		System.out.println("Waiting....");
-		Thread.sleep(5000);
-		System.out.println("Exiting....Exited");
+			System.out.println("Waiting....");
+			Thread.sleep(5000);
+			System.out.println("Exiting....Exited");
 		
 		//exit;
-		at.quitDriver();
+			at.quitDriver();
 	}
 
 }
