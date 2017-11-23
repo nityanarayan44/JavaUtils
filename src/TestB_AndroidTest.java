a8fbdab749e8fe33a02d066acd71fc65ea4ae4bc
nityanarayan44 <nityanarayan44@live.com>
2017-11-23 14:30:27 +0530

    Sample Class for android test is updated

--- src/TestB_AndroidTest.java
+++ src/TestB_AndroidTest.java
@@ -5,6 +5,7 @@
  * Implements the AndroidDriver.
  * this is a sample test, Derives a sample Calculator Addition.
  * 
+ * Appium Server: 1.6.5
  * Jar Files: 	Selenium Server Stnadalone 3.4.0
  * 				appium java-client-5.0.0-BETA9
  * 				guava-22.0
@@ -18,6 +19,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.nng.automation.utils.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,8 +36,9 @@ public class TestB_AndroidTest {
 	  @Test (priority = 1, enabled = true)
 	  public void f() throws Exception {
 		// Wait
-			Thread.sleep(5000);  
-		// Operation [2+9=]
+			Thread.sleep(5000);
+			
+		// Normal Operation [2+9=]
 			this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/digit_2']")).click();
 			this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/op_add']")).click();
 			this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/digit_9']")).click();
@@ -43,10 +46,30 @@ public class TestB_AndroidTest {
 		// Get the Result [Expected sum Result:  11].
 			System.out.println("Result: " + this.driver.androidDriver.findElement(By.xpath("//android.widget.TextView[@resource-id='com.android.calculator2:id/result']")).getText());
 			Assert.assertEquals("11", this.driver.androidDriver.findElement(By.xpath("//android.widget.TextView[@resource-id='com.android.calculator2:id/result']")).getText() );
+		
 		// Wait
 			Thread.sleep(5000);  
-					
-		
+			
+		// Special Operations [ cos(90) calculation]
+			// Swipe from Right to Left;
+				Dimension dim = this.driver.androidDriver.manage().window().getSize();
+				System.out.printf("\n Current Dimentions: Width=%d, Height=%d \n", dim.width, dim.height);
+				this.driver.androidDriver.swipe( 1020, 695, 30, 265, 10);
+			// Choose Cos function
+				this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/fun_cos']")).click();
+			// slide back
+				// [240,693][1080,1794] [Exact value.]
+				// this.driver.androidDriver.pinch(150, 690);
+				this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/digit_7']")).click();
+				Thread.sleep(3000);
+			// choose a number to get its cos value.
+				this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/digit_9']")).click();
+				this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/digit_0']")).click();
+				this.driver.androidDriver.findElement(By.xpath("//android.widget.Button[@resource-id='com.android.calculator2:id/eq']")).click();
+				
+		// Wait to exit
+			Thread.sleep(5000);  
+			return;
 	  }
  
 	/* ==============================
@@ -64,6 +87,8 @@ public class TestB_AndroidTest {
 			opt.put("deviceName", 		"emulator-5554");
 			opt.put("appPackage", 		"com.android.calculator2");
 			opt.put("appActivity",		"com.android.calculator2.Calculator");
+			opt.put("autoGrantPermissions","true"); // Automatically grant the permission. No popups
+			
 			this.driver.getAndroidDriver(opt);
 			return;
 	  }
